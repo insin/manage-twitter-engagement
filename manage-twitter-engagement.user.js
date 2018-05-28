1b7bdd0e5379d46c435c6a0ee1dddc7b584b86f1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2018-05-28 22:11:17 +1000

    Support managing engagement when navigating to the home page

--- manage-twitter-engagement.user.js
+++ manage-twitter-engagement.user.js
@@ -2,8 +2,8 @@
 // @name        Manage Twitter Engagement
 // @description Manage extra "engagement" on Twitter by hiding Retweets and algorithmic timeline tweets (Retweetlikes)
 // @namespace   https://github.com/insin/manage-twitter-engagement/
-// @match       https://twitter.com/
-// @version     2
+// @match       https://twitter.com/*
+// @version     3
 // ==/UserScript==
 
 const CIRCLE = 'display: inline-block; width: 20px; height: 20px; vertical-align: text-bottom; border-radius: 50%'
@@ -13,73 +13,84 @@ const RETWEET_SELECTOR = 'div[data-retweet-id]'
 // Identify retweetlikes by the heart icon in their context header
 const RETWEETLIKE_SELECTOR = '.tweet-context .Icon--heartBadge'
 
-let retweetBG
-let retweetlikeBG
-
-function setThemeColours(isNightMode) {
-  retweetBG = `background-color: ${isNightMode ? '#1b3036' : '#f5fcf8'}`
-  retweetlikeBG = `background-color: ${isNightMode ? '#2f2836' : '#fdf6f8'}`
-}
-
-setThemeColours(/nightmode/.test(document.querySelector('link[class="coreCSSBundles"]').href))
-
 // Default to hiding Retweetlikes because GTFO, Twitter
 let hideRetweets = localStorage.mte_hideRetweets === 'true'
 let hideRetweetLikes = localStorage.mte_hideRetweetLikes === undefined || localStorage.mte_hideRetweetLikes === 'true'
 
+// UI variables
 let retweetCount = 0
 let retweetLikeCount = 0
+let streamObserver = null
+let styleModeObserver = null
+let retweetBG
+let retweetlikeBG
+let $retweetCount
+let $retweetPlural
+let $retweetLikeCount
+let $retweetLikePlural
 
 let getAllTweets = () => document.querySelectorAll('#stream-items-id > .stream-item')
 
-let $controls = document.createElement('div')
-$controls.style = 'padding: 0 16px 16px 16px'
-$controls.innerHTML = `
-  <div style="margin-bottom: 3px">
-    <label>
-      <input type="checkbox" class="mte_hideRetweets">
-      Hide <span class="mte_retweetCount">0</span>
-      Retweet<span class="mte_retweetPlural">s</span>
-      <span class="mte_retweetColour" style="${retweetBG}; ${CIRCLE}"></span>
-    </label>
-  </div>
-  <div>
-    <label>
-      <input type="checkbox" class="mte_hideRetweetLikes">
-      Hide <span class="mte_retweetLikeCount">0</span>
-      Retweetlike<span class="mte_retweetLikePlural">s</span>
-      <span class="mte_retweetlikeColour" style="${retweetlikeBG}; ${CIRCLE}"></span>
-    </label>
-  </div>
-`
+function injectUI() {
+  let $controls = document.createElement('div')
+  $controls.style = 'padding: 0 16px 16px 16px'
+  $controls.innerHTML = `
+    <div style="margin-bottom: 3px">
+      <label>
+        <input type="checkbox" class="mte_hideRetweets">
+        Hide <span class="mte_retweetCount">${retweetCount}</span>
+        Retweet<span class="mte_retweetPlural">${retweetCount === 1 ? '' : 's'}</span>
+        <span class="mte_retweetColour" style="${retweetBG}; ${CIRCLE}"></span>
+      </label>
+    </div>
+    <div>
+      <label>
+        <input type="checkbox" class="mte_hideRetweetLikes">
+        Hide <span class="mte_retweetLikeCount">${retweetLikeCount}</span>
+        Retweetlike<span class="mte_retweetLikePlural">${retweetLikeCount === 1 ? '' : 's'}</span>
+        <span class="mte_retweetlikeColour" style="${retweetlikeBG}; ${CIRCLE}"></span>
+      </label>
+    </div>
+  `
 
-let $hideRetweets = $controls.querySelector('.mte_hideRetweets')
-let $retweetCount = $controls.querySelector('.mte_retweetCount')
-let $retweetPlural = $controls.querySelector('.mte_retweetPlural')
-$hideRetweets.checked = hideRetweets
-$hideRetweets.addEventListener('click', (e) => {
-  hideRetweets = e.target.checked
-  localStorage.mte_hideRetweets = hideRetweets
-  for (let tweet of getAllTweets()) {
-    if (tweet.querySelector(RETWEET_SELECTOR)) {
-      tweet.style.display = hideRetweets ? 'none' : ''
+  let $hideRetweets = $controls.querySelector('.mte_hideRetweets')
+  $retweetCount = $controls.querySelector('.mte_retweetCount')
+  $retweetPlural = $controls.querySelector('.mte_retweetPlural')
+  $hideRetweets.checked = hideRetweets
+  $hideRetweets.addEventListener('click', (e) => {
+    hideRetweets = e.target.checked
+    localStorage.mte_hideRetweets = hideRetweets
+    for (let tweet of getAllTweets()) {
+      if (tweet.querySelector(RETWEET_SELECTOR)) {
+        tweet.style.display = hideRetweets ? 'none' : ''
+      }
     }
-  }
-})
+  })
 
-let $hideRetweetLikes = $controls.querySelector('.mte_hideRetweetLikes')
-let $retweetLikeCount = $controls.querySelector('.mte_retweetLikeCount')
-let $retweetLikePlural = $controls.querySelector('.mte_retweetLikePlural')
-$hideRetweetLikes.checked = hideRetweetLikes
-$hideRetweetLikes.addEventListener('click', (e) => {
-  hideRetweetLikes = e.target.checked
-  localStorage.mte_hideRetweetLikes = hideRetweetLikes
-  for (let tweet of getAllTweets()) {
-    if (tweet.querySelector(RETWEETLIKE_SELECTOR)) {
-      tweet.style.display = hideRetweetLikes ? 'none' : ''
+  let $hideRetweetLikes = $controls.querySelector('.mte_hideRetweetLikes')
+  $retweetLikeCount = $controls.querySelector('.mte_retweetLikeCount')
+  $retweetLikePlural = $controls.querySelector('.mte_retweetLikePlural')
+  $hideRetweetLikes.checked = hideRetweetLikes
+  $hideRetweetLikes.addEventListener('click', (e) => {
+    hideRetweetLikes = e.target.checked
+    localStorage.mte_hideRetweetLikes = hideRetweetLikes
+    for (let tweet of getAllTweets()) {
+      if (tweet.querySelector(RETWEETLIKE_SELECTOR)) {
+        tweet.style.display = hideRetweetLikes ? 'none' : ''
+      }
     }
+  })
+
+  let $profileCard = document.querySelector('div.ProfileCardStats')
+  if ($profileCard) {
+    $profileCard.insertAdjacentElement('afterend', $controls)
   }
-})
+}
+
+function setThemeColours({nightMode}) {
+  retweetBG = `background-color: ${nightMode ? '#1b3036' : '#f5fcf8'}`
+  retweetlikeBG = `background-color: ${nightMode ? '#2f2836' : '#fdf6f8'}`
+}
 
 function updateCounts() {
   $retweetCount.innerHTML = retweetCount
@@ -105,45 +116,93 @@ function processTweets(tweets) {
       }
     }
   }
-  updateCounts()
 }
 
-// Deal with the initial batch of tweets
-processTweets(getAllTweets())
-
-// Watch the stream for the appearance of new tweets
-let streamItems = document.getElementById('stream-items-id')
-new MutationObserver(mutations =>
-  mutations.forEach(mutation => processTweets(mutation.addedNodes))
-).observe(streamItems, {childList: true})
-
-// Watch <head> for the core CSS bundle changing when nightmode is toggled
-new MutationObserver(mutations =>
-  mutations.forEach(mutation => {
-    if (mutation.addedNodes.length === 0 ||
-        mutation.addedNodes[0].getAttribute('class') !== 'coreCSSBundles') {
-      return
-    }
-
-    // Update retweet and retweetlike highlight colours
-    setTimeout(() => {
-      setThemeColours(/nightmode/.test(mutation.addedNodes[0].href))
-      document.querySelector('.mte_retweetColour').style = `${retweetBG}; ${CIRCLE}`
-      document.querySelector('.mte_retweetlikeColour').style = `${retweetlikeBG}; ${CIRCLE}`
-      for (tweet of getAllTweets()) {
-        if (tweet.querySelector(RETWEETLIKE_SELECTOR)) {
-          tweet.style = retweetlikeBG
-        }
-        else if (tweet.querySelector(RETWEET_SELECTOR)) {
-          tweet.style = retweetBG
-        }
-      }
-    }, 500)
+function startManagingEngagement() {
+  // Reset runtime variables
+  retweetCount = 0
+  retweetLikeCount = 0
+  setThemeColours({
+    nightMode: /nightmode/.test(document.querySelector('link[class="coreCSSBundles"]').href)
   })
-).observe(document.querySelector('head'), {childList: true, attributes: true})
 
-// Show controls
-let $profileCard = document.querySelector('div.ProfileCardStats')
-if ($profileCard) {
-  $profileCard.insertAdjacentElement('afterend', $controls)
+  // Deal with the initial batch of tweets
+  processTweets(getAllTweets())
+
+  // Watch the stream for the appearance of new tweets
+  streamObserver = new MutationObserver(mutations =>
+    mutations.forEach(mutation => {
+      processTweets(mutation.addedNodes)
+      updateCounts()
+    })
+  )
+  streamObserver.observe(document.getElementById('stream-items-id'), {
+    childList: true
+  })
+
+  // Watch <head> for the core CSS bundle changing when nightmode is toggled
+  styleModeObserver = new MutationObserver(mutations =>
+    mutations.forEach(mutation => {
+      if (mutation.addedNodes.length === 0 ||
+          mutation.addedNodes[0].getAttribute('class') !== 'coreCSSBundles') {
+        return
+      }
+
+      // Update retweet and retweetlike highlight colours
+      setTimeout(() => {
+        setThemeColours({
+          nightMode: /nightmode/.test(mutation.addedNodes[0].href)
+        })
+        document.querySelector('.mte_retweetColour').style = `${retweetBG}; ${CIRCLE}`
+        document.querySelector('.mte_retweetlikeColour').style = `${retweetlikeBG}; ${CIRCLE}`
+        for (tweet of getAllTweets()) {
+          if (tweet.querySelector(RETWEETLIKE_SELECTOR)) {
+            tweet.style = retweetlikeBG
+          }
+          else if (tweet.querySelector(RETWEET_SELECTOR)) {
+            tweet.style = retweetBG
+          }
+        }
+      }, 500)
+    })
+  )
+  styleModeObserver.observe(document.querySelector('head'), {
+    attributes: true,
+    childList: true,
+  })
+
+  // Show controls
+  injectUI()
 }
+
+function stopManagingEngagement() {
+  streamObserver.disconnect()
+  streamObserver = null
+  styleModeObserver.disconnect()
+  styleModeObserver = null
+}
+
+/**
+ * When you navigate between sections, the Twitter website loads HTML for the
+ * next page and injects it.
+ *
+ * Check for page changes by observing <body>'s class attribute, which is
+ * updated with loading state classes and after loading, for non-homepage pages,
+ * a <Name>Page class.
+ */
+new MutationObserver(() => {
+  // Ignore loading states
+  if (/(swift-loading|pushing-state)/.test(document.body.className)) return
+
+  let page = /(\w+)Page/.exec(document.body.className)
+
+  // The homepage has no <Name>Page class
+  if (!page) {
+    if (streamObserver == null) {
+      startManagingEngagement()
+    }
+  }
+  else if (streamObserver) {
+    stopManagingEngagement()
+  }
+}).observe(document.body, {attributes: true, attributeFilter: ['class']})
